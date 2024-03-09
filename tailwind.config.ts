d349073a1a47b7da9d73956d91b210a1a6c1aa24
Jadurani Davalos <jadurani.davalos@gmail.com>
2024-03-09 13:22:36 -0500

    feat: cleanups

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,27 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      mobile: "375px",
+      desktop: "1440px"
+    },
+    fontSize: {
+      body2: "0.8rem",
+      body1: "1rem",
+      h6: "1rem",
+      h5: "1.25rem",
+      h4: "1.563rem",
+      h3: "1.953rem",
+      h2: "2.441rem",
+      h1: "3.052rem",
+    },
     extend: {
+      colors: {
+        red: "#FF7979",
+        green: "#38CC8B",
+        purple: "#5E54A4",
+        neutral: "#3D3B48"
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
